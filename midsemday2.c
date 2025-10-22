ae1378e0a3718989774f137b8b9e6cbf110537f1
roshini-akula <roshiniakula04@gmail.com>
2025-10-22 23:30:46 +0530

    Create midsemday2.c

---
+++ midsemday2.c
@@ -0,0 +1,157 @@
+//eligible for vote 
+// #include <stdio.h>
+// int main()
+// {
+//     int a;
+//     printf("enter your age:");
+//     scanf("%d",&a);
+//     if(a>=18){
+//         printf("you are eligible for vote");
+//     }
+//     else
+//     {
+//         printf("you are not eligible for vote");
+//     }
+//     return 0;
+// }
+
+
+//largest of three numbers
+
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a,b,c;
+//     printf("enter three numbers:");
+//     scanf("%d%d%d",&a,&b,&c);
+//     if(a>=b && a>=c)
+//     {
+//         printf("the largest number is %d\n",a);
+//     }
+//     else if(b>=a && b>=c)
+//     {
+//         printf("the largest number is %d\n",b);
+//     }
+//     else
+//     {
+//         printf("the largest number is %d\n",c);
+//     }
+//     return 0;
+// }
+
+
+// THREE ANGLES TO CHECK WHETHER THE GIVEN IS EQUILATERAL TRIANGLE
+
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a1,a2,a3;
+//     printf("enter the angle values:");
+//     scanf("%d%d%d",&a1,&a2,&a3);
+//     if(a1+a2+a3!=180)
+//     {
+//         printf("the angles do not forms an equilateral triangle./n");
+//     }
+//     else if(a1==60 && a2==60 && a3==60)
+//     {
+//         printf("the angles form an equilateral triangle./n");
+//     }
+//     else
+//     {
+//         printf("the angles form a triangle but not a equilateral triangle");
+//     }
+//     return 0;
+// }
+
+
+//TO HCECK THE PROGRAM WHETHER THE CHARACTER IS ALPHABET,DIGIT,SPER=CIAL CHARACTER
+
+// #include <stdio.h>
+// int main()
+// {
+//     char ch;
+//     printf("enter a character:");
+//     scanf("%c",&ch);
+//     if((ch>='A' && ch<='Z')||(ch>='a' && ch<='z'))
+//     {
+//         printf("%c is an alphabet",ch);
+//     }
+//     else if(ch>='0' && ch<='9')
+//     {
+//         printf("%c is a digit",ch);
+//     }
+//     else
+//     {
+//         printf("%c is a special charcter",ch);
+//     }
+//     return 0;
+// }
+
+
+//TO CHECK WHETHER THE GIVEN NUMBER IS POSITIVE OR NEGATIVE OR NEUTRAL
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     printf("Enter a number:");
+//     scanf("%d",&n);
+//     if(n>0)
+//     {
+//         printf("%d is positive./n",n);
+//     }
+//     else if(n<0)
+//     {
+//         printf("%d is negative./n",n);
+//     }
+//     else
+//     {
+//         printf("the number is neutral./n",n);
+//     }
+//     return 0;
+// }
+
+//TO TAKE A INPUT AS A NUMBER 1 TO 7
+
+// #include <stdio.h>
+// int main()
+// {
+//     int day;
+//     printf("enter the value:(1 to 7)");
+//     scanf("%d",&day);
+//     if(day==1)
+//     {
+//         printf("monday",day);
+//     }
+//     else if(day==2)
+//     {
+//         printf("tuesday",day);
+//     }
+//     else if(day==3)
+//     {
+//         printf("wednesday",day);
+//     }
+//     else if(day==4)
+//     {
+//         printf("thursday",day);
+//     }
+//     else if(day==5)
+//     {
+//         printf("friday",day);
+//     }
+//     else if(day==6)
+//     {
+//         printf("saturday",day);
+//     }
+//     else if(day==7)
+//     {
+//         printf("sunday",day);
+//     }
+//     else
+//     {
+//         printf("invalid number:please enter correct number");
+//     }
+//     return 0;
+// }
+
